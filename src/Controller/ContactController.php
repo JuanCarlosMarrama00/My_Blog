cf8ffb303826fffd39b09e23aae8b6078f227a60
JuanCarlosMarrama00 <marramamartinezjc@gmail.com>
2023-11-03 10:26:09 +0100

    He añadido funcionalidad al formulario

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,15 +2,35 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
+use App\Form\ContactFormType;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'contact')]
-    public function index(): Response
+    public function contact(ManagerRegistry $doctrine, Request $request): Response
+    {   
+        $contact = new Contact();
+        $form = $this->createForm(ContactFormType::class, $contact);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $contacto = $form->getData();
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($contacto);
+            $entityManager->flush();
+            return $this->redirectToRoute('thankyou', []);
+        }
+        return $this->render('contact/index.html.twig', ['form'=>$form->createView()]);
+    }
+
+    #[Route('/thanyou', name:'thankyou')]
+    public function thankyou():Response
     {
-        return $this->render('contact/index.html.twig', []);
+        return $this->render('thankyou.html.twig', []);
     }
 }
---
+++ src/Entity/Contact.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ContactRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: ContactRepository::class)]
+class Contact
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message:"Field `Name` is mandatory")]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 9)]
+    #[Assert\NotBlank(message:"Field `phoneNumber` is mandatory")]
+    private ?string $phoneNumber = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message:"Field `email` is mandatory")]
+    #[Assert\Email(message:"{{ value }} is not a valid email")]
+    private ?string $email = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message:"Field `message` is mandatory")]
+    private ?string $message = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPhoneNumber(): ?string
+    {
+        return $this->phoneNumber;
+    }
+
+    public function setPhoneNumber(string $phoneNumber): static
+    {
+        $this->phoneNumber = $phoneNumber;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): static
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+}
